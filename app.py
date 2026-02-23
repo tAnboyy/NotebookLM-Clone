2985b98bbbfc85f4f4db2a6e3ebd43752dfd988e
mdasthanmay <mdasthanmay@gmail.com>
2026-02-23 18:20:14 -0500

    testing Hugging Face action

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import gradio as gr
 
 def greet(name):
-    return "Hello " + name + "!!"
+    return "Hello " + name + "!!. Testing Hugging Face deployment!"
 
 demo = gr.Interface(fn=greet, inputs="text", outputs="text")
 demo.launch()
